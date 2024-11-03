344dae3f51963864299140940a8941fceddd66f6
Mohdlateef <lateefahbaba123@gmail.com>
2024-11-03 12:12:43 +0530

    first half done

--- example 4/index.js
+++ example 4/index.js
@@ -3,7 +3,7 @@ const express=require("express");
 const app=express();
 
 const fs=require("fs")
-
+app.use(express.json())
 app.get("/home",(req,res)=>{
     return res.json({
         "key1":"we are in phase 1",
@@ -12,9 +12,36 @@ app.get("/home",(req,res)=>{
     })
 })
 
-app.get("getfile",(req,res)=>{
-    
+app.get("/getfile",(req,res)=>{
+    fs.readFile('file1.json',(err,data)=>{
+        if(err){
+            throw err;
+        }
+        return res.send(data)
+    })
 })
+
+
+// writefile
+
+
+let data="we are good state but i am trying to get how to make josn file"
+
+
+app.post("/writefile",(req,res)=>{
+    console.log(req.body)
+    fs.writeFile(`${req.body.filename}`,req.body.data,(err)=>{
+        if (err){
+            throw err
+        }
+    })
+    return res.send('write file has been done sucessfully')
+})
+
+
+
+
+
 
 app.listen(8000,()=>{
     console.log("we are running on port 8000")
