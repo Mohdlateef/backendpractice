fabbff7d8cbea9c2d50c4323dffc9fef15968c3d
Mohdlateef <lateefahbaba123@gmail.com>
2024-11-07 11:52:51 +0530

    webtoken done

--- example7_webtoken/index.js
+++ example7_webtoken/index.js
@@ -1,9 +1,13 @@
 const express=require("express");
 require("dotenv").config();
 const mongoose=require("mongoose");
-
+const jwt=require("jsonwebtoken")
+const nodemailer=require("nodemailer")
 const app=express();
  
+// Global_Middlewares
+app.use(express.json());
+
 // file_Exports
 const usermodel = require("./models/usermodel");
 
@@ -14,21 +18,76 @@ const usermodel = require("./models/usermodel");
 
 // Register Api
 
-app.post("/Register",async (req,res)=>{
+app.post("/register",async (req,res)=>{
     const {name,email,username,password}=req.body;
-const userData=await usermodel.findone()
+    try{
+    const userData=await usermodel.findOne({email:email})
+    // if(userData){
+    //     return res.send("user already register");
+    // }
+    console.log(userData);
     const userdb= new usermodel({
         name,
         email,
         username,
         password,
     })
-       await userdb.save()
+
+    // generate_WebToken
+    const token=jwt.sign({email},process.env.SECRET)
+
+    // sendtoken
+    const transporter=nodemailer.createTransport({
+        host:"smtp.gmail.com",
+        port:465,
+        secure:true,
+        service:"gamil",
+        auth:{
+            user:"lateefahbaba123@gmail.com",
+            pass:"obdp fhkj zeuj ewaf",
+            }
+    })
+const mailOptions={
+    from:"lateefahbaba123@gmail.com",
+    to:email,
+    subject:"email verificatin for token gen",
+    html:`<a href="http://localhost:8000/verify?token=${token}">click me to verify your email</a>`
+}
+transporter.sendMail(mailOptions,function(error,info)
+{
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(`email has been sent sucessfully:${email}`);
+    }
+})
+
+// console.log(token);
+// const data= jwt.verify(token,process.env.SECRET)
+
+    //    await userdb.save()
+       return res.send("user register sucessfully");
+    } catch (error) {
+        console.log(error);
+        return res.send(error)
+    }
+
 })
 
 
 
+// verify token
 
+app.get('/verify',async(req,res)=>{
+    const token =req.query.token;
+    console.log()
+    console.log(token)
+ const userinfo= jwt.verify(token,process.env.SECRET)
+ 
+await usermodel.findOneAndUpdate({})
+ res.send("email has been verified sucessfully");
+})
 
 
 
